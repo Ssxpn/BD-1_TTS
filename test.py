f0b969bc197eea15c58987fc2d97df6dc3c56e3e
Ssxpn <pierrot2109@gmail.com>
2025-03-28 17:21:25 +0100

    Modification prise en compte mots plus petits

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ from text_to_speech_v2 import tts_bd1  # Import de la fonction
 
 # 🔊 **Test**
 if __name__ == "__main__":
-    tts_bd1("Bonjour à toi ! Comment vas-tu ?")
+    tts_bd1("Quel est ton nom ?")
